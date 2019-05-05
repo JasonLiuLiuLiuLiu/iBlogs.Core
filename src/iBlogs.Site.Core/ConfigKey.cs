7477fe9c1ff93487ee0a85780d6ef22c0a2cfeca
Zhenyu,Liu <liuzhenyulive@live.com>
2019-05-05 15:37:10 +0800

    Add resource

---
+++ src/iBlogs.Site.Core/ConfigKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iBlogs.Site.Core
+{
+    public class ConfigKey
+    {
+        public static string SqliteDbFileName = "SqliteDbFileName";
+    }
+}
---
+++ src/iBlogs.Site.Core/SqLite/SqLiteBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using iBlogs.Site.Common.Extensions;
+using iBlogs.Site.Core.Configuration;
+using Microsoft.Data.Sqlite;
+using System;
+
+namespace iBlogs.Site.Core.SqLite
+{
+    public class SqLiteBaseRepository
+    {
+        public static string DbFile
+        {
+            get { return Environment.CurrentDirectory + AppConfigurations.Get()[ConfigKey.SqliteDbFileName].IfNullReturnDefaultValue("iBlogs.db"); }
+        }
+
+        public static SqliteConnection SimpleDbConnection()
+        {
+            return new SqliteConnection("Data Source=" + DbFile);
+        }
+    }
+}
