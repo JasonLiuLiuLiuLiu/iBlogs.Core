7436bc8bc29d19583011495788864f71574aa256
Zhenyu,Liu <liuzhenyulive@live.com>
2019-05-14 22:55:37 +0800

    Add other pages

---
+++ src/iBlogs.Site.Web/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,15 @@
+﻿using iBlogs.Site.Web.Attribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iBlogs.Site.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentsController : Controller
+    {
+        [ViewLayout("~/Areas/Admin/Views/Layout/Layout.cshtml")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/iBlogs.Site.Web/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iBlogs.Site.Web.Attribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iBlogs.Site.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SettingController : Controller
+    {
+        [ViewLayout("~/Areas/Admin/Views/Layout/Layout.cshtml")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
