88521a972b1cc4694cb55a7b821273c382767e85
Zhenyu,Liu <liuzhenyulive@live.com>
2019-05-14 22:30:55 +0800

    Add attaches

---
+++ src/iBlogs.Site.Web/Areas/Admin/Controllers/AttachesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iBlogs.Site.Web.Attribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iBlogs.Site.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AttachesController : Controller
+    {
+        [ViewLayout("~/Areas/Admin/Views/Layout/Layout.cshtml")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
