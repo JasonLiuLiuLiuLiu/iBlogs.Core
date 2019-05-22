966c722c3013d58e00cbb5452dd8f43b2dada152
Zhenyu,Liu <liuzhenyulive@live.com>
2019-05-23 00:00:02 +0800

    Update iBlogsContext.cs

--- src/iBlogs.Site.Core/EntityFrameworkCore/iBlogsContext.cs
+++ src/iBlogs.Site.Core/EntityFrameworkCore/iBlogsContext.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Mail;
+using iBlogs.Site.Core.Comment;
+using iBlogs.Site.Core.Content;
+using iBlogs.Site.Core.Log;
+using iBlogs.Site.Core.Meta;
+using iBlogs.Site.Core.Option;
+using iBlogs.Site.Core.Relationship;
+using iBlogs.Site.Core.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace iBlogs.Site.Core.EntityFrameworkCore
@@ -11,5 +18,15 @@ namespace iBlogs.Site.Core.EntityFrameworkCore
         {
 
         }
+
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Contents> Contents { get; set; }
+        public DbSet<Logs> Logs { get; set; }
+        public DbSet<Metas> Metas { get;set;}
+        public DbSet<Options> Options { get; set; }
+        public DbSet<Relationships> Relationships { get; set; }
+        public DbSet<Users> Users { get; set; }
+
     }
 }
