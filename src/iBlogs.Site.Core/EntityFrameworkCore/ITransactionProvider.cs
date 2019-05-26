a16bd9c0b8670598317bd1402bdcd8bcdbbd9109
Zhenyu,Liu <liuzhenyulive@live.com>
2019-05-26 16:34:37 +0800

    Add transaction provider

---
+++ src/iBlogs.Site.Core/EntityFrameworkCore/ITransactionProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace iBlogs.Site.Core.EntityFrameworkCore
+{
+    public interface ITransactionProvider
+    {
+        IDbContextTransaction CreateTransaction();
+        void Dispose();
+    }
+}
---
+++ src/iBlogs.Site.Core/EntityFrameworkCore/TransactionProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace iBlogs.Site.Core.EntityFrameworkCore
+{
+    public class TransactionProvider:IDisposable, ITransactionProvider
+    {
+        private readonly iBlogsContext _blogsContext;
+
+        public TransactionProvider(iBlogsContext blogsContext)
+        {
+            _blogsContext = blogsContext;
+        }
+
+        public List<IDbContextTransaction> Transactions=new List<IDbContextTransaction>();
+
+        public IDbContextTransaction CreateTransaction()
+        {
+            var tra=_blogsContext.Database.BeginTransaction();
+            Transactions.Add(tra);
+            return tra;
+        }
+
+        public void Dispose()
+        {
+            for (var i = Transactions.Count-1; i >= 0; i--)
+            {
+                Transactions[i].Dispose();
+            }
+        }
+    }
+}
