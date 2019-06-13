325ff37a7d45e0aa1f8717cd5bdff5d04e527143
Zhenyu,Liu <liuzhenyulive@live.com>
2019-06-13 22:36:29 +0800

    Add search function

--- src/iBlogs.Site.Web/Controllers/IndexController.cs
+++ src/iBlogs.Site.Web/Controllers/IndexController.cs
@@ -25,7 +25,7 @@ namespace iBlogs.Site.Web.Controllers
         {
             var articleParam = new ArticleParam
             {
-                Page = index==0?1:index
+                Page = index == 0 ? 1 : index
             };
             return View(new IndexViewModel
             {
@@ -39,7 +39,7 @@ namespace iBlogs.Site.Web.Controllers
         [ViewLayout("~/Views/Layout/Layout.cshtml")]
         public IActionResult Hot(int index)
         {
-            var articleParam = new ArticleParam {Page = index == 0 ? 1 : index, OrderBy = "Hits"};
+            var articleParam = new ArticleParam { Page = index == 0 ? 1 : index, OrderBy = "Hits" };
             return View("Index", new IndexViewModel
             {
                 Contents = _contentsService.FindArticles(articleParam),
@@ -52,7 +52,7 @@ namespace iBlogs.Site.Web.Controllers
         [ViewLayout("~/Views/Layout/Layout.cshtml")]
         public IActionResult Random(int index)
         {
-            var articleParam = new ArticleParam {Page = index == 0 ? 1 : index, OrderBy = "Random"};
+            var articleParam = new ArticleParam { Page = index == 0 ? 1 : index, OrderBy = "Random" };
             return View("Index", new IndexViewModel
             {
                 Contents = _contentsService.FindArticles(articleParam),
@@ -63,21 +63,21 @@ namespace iBlogs.Site.Web.Controllers
         [HttpGet("/index/tag/{tag}/{index}")]
         [HttpGet("/tag/{tag}")]
         [ViewLayout("~/Views/Layout/Layout.cshtml")]
-        public IActionResult Tag(string tag,int index)
+        public IActionResult Tag(string tag, int index)
         {
             return View("Index", new IndexViewModel
             {
                 DisplayType = "标签",
                 DisplayMeta = tag,
                 OrderType = "tag",
-                Contents = _contentsService.FindContentByMeta(MetaType.Tag, tag, new ArticleParam { Page = index==0?1:index})
+                Contents = _contentsService.FindContentByMeta(MetaType.Tag, tag, new ArticleParam { Page = index == 0 ? 1 : index })
             });
         }
 
         [HttpGet("/index/category/{category}/{index}")]
         [HttpGet("/category/{category}")]
         [ViewLayout("~/Views/Layout/Layout.cshtml")]
-        public IActionResult Category(string category,int index)
+        public IActionResult Category(string category, int index)
         {
             return View("Index", new IndexViewModel
             {
@@ -86,7 +86,7 @@ namespace iBlogs.Site.Web.Controllers
                 OrderType = "category",
                 Contents = _contentsService.FindContentByMeta(MetaType.Category, category, new ArticleParam
                 {
-                    Page = index==0?1:index
+                    Page = index == 0 ? 1 : index
                 })
             });
         }
@@ -98,7 +98,7 @@ namespace iBlogs.Site.Web.Controllers
         {
             return View(_contentsService.GetArchive(new PageParam
             {
-                Page = index==0?1:index
+                Page = index == 0 ? 1 : index
             }));
         }
 
@@ -114,6 +114,27 @@ namespace iBlogs.Site.Web.Controllers
         public IActionResult AllCategories()
         {
             return View("AllCategories", _metasService.LoadMetaDataViewModel(MetaType.Category));
+        }
+
+        [HttpGet("search")]
+        [HttpGet("/search/{keyword}")]
+        [HttpGet("/search/{keyword}/{index}")]
+        [ViewLayout("~/Views/Layout/Layout.cshtml")]
+        public IActionResult Search(string keyword, int index)
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+                return View("Index", new IndexViewModel
+                {
+                    OrderType = "Index",
+                    Contents = _contentsService.FindArticles(new ArticleParam { Page = index == 0 ? 1 : index })
+                });
+
+            var articleParam = new ArticleParam { Page = index == 0 ? 1 : index, Title = keyword };
+            return View("Search", new SearchViewModel
+            {
+                KeyWord = keyword,
+                Contents = _contentsService.FindArticles(articleParam)
+            });
         }
     }
 }
---
+++ src/iBlogs.Site.Web/Models/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using iBlogs.Site.Core.Common.Response;
+using iBlogs.Site.Core.Content.DTO;
+
+namespace iBlogs.Site.Web.Models
+{
+    public class SearchViewModel
+    {
+        public string KeyWord { get; set; }
+        public Page<ContentResponse> Contents { get; set; }
+    }
+}
