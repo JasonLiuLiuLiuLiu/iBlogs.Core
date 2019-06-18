5e29abfaa9a328e62b7b724d681ff0ca6cb47b51
Zhenyu,Liu <liuzhenyulive@live.com>
2019-06-18 23:41:10 +0800

    add  announcement

--- src/iBlogs.Site.Core/Common/Request/PageParam.cs
+++ src/iBlogs.Site.Core/Common/Request/PageParam.cs
@@ -3,7 +3,7 @@
     public class PageParam
     {
         public int Page { get; set; } = 1;
-        public int Limit { get; set; } = 5;
+        public int Limit { get; set; } = 10;
         public string OrderBy { get; set; }
         public OrderType OrderType { get; set; }
     }
