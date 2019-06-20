0b613bef0e67bcb1192f2332eb7dcc48b8130bdf
Zhenyu,Liu <liuzhenyulive@live.com>
2019-06-20 22:26:48 +0800

    Add exception filter

---
+++ src/iBlogs.Site.Web/Filter/ExceptionFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iBlogs.Site.Core.Common.Response;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace iBlogs.Site.Web.Filter
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+
+        public ExceptionFilter(
+            IHostingEnvironment hostingEnvironment,
+            IModelMetadataProvider modelMetadataProvider)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            if (!_hostingEnvironment.IsDevelopment())
+            {
+                return;
+            }
+
+            if (context.HttpContext.Request.GetDisplayUrl().Contains("api"))
+            {
+                context.Result = new ObjectResult(ApiResponse.Fail(context.Exception.Message));
+                return;
+            }
+
+            var result = new ViewResult
+            {
+                StatusCode = 500,
+                ViewName = "CustomError",
+                ViewData = new ViewDataDictionary(_modelMetadataProvider, context.ModelState) { { "Exception", context.Exception } }
+            };
+            context.Result = result;
+        }
+    }
+}
