7e16dd74b9a25fa32fb4a559169df216636e16db
Zhenyu,Liu <liuzhenyulive@live.com>
2019-06-20 22:34:23 +0800

    Update ExceptionFilter.cs

--- src/iBlogs.Site.Web/Filter/ExceptionFilter.cs
+++ src/iBlogs.Site.Web/Filter/ExceptionFilter.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using iBlogs.Site.Core.Common.Response;
+﻿using iBlogs.Site.Core.Common.Response;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace iBlogs.Site.Web.Filter
 {
     public class ExceptionFilter : ExceptionFilterAttribute
     {
         private readonly IHostingEnvironment _hostingEnvironment;
-        private readonly IModelMetadataProvider _modelMetadataProvider;
 
-        public ExceptionFilter(
-            IHostingEnvironment hostingEnvironment,
-            IModelMetadataProvider modelMetadataProvider)
+        public ExceptionFilter(IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
-            _modelMetadataProvider = modelMetadataProvider;
         }
 
         public override void OnException(ExceptionContext context)
@@ -38,7 +28,7 @@ namespace iBlogs.Site.Web.Filter
                 return;
             }
 
-            context.Result =new StatusCodeResult(500);
+            context.Result = new StatusCodeResult(500);
         }
     }
 }
