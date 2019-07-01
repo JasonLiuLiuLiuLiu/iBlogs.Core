167f35de26029a6e5852b1361255b83257282a9e
Zhenyu,Liu <liuzhenyulive@live.com>
2019-07-01 20:13:47 +0800

    Update DeployForInstalled.sh

--- deploy/DeployForInstalled.sh
+++ deploy/DeployForInstalled.sh
@@ -1,4 +1,4 @@
-docker pull liuzhenyulive/iblogs
-docker stop iblogs
-docker rm iblogs
-docker run -d -p 80:80 --restart=always --name iblogs liuzhenyulive/iblogs DbServer=$DbServer DbName=$DbName DbUID=$DbUID DbPWD=$DbPWD
+docker pull $(dockerId)/$(imageName):$(Build.BuildNumber)
+docker stop $(imageName)
+docker rm $(imageName)
+docker run -d -p 80:80 --restart=always --name $(imageName) $(dockerId)/$(imageName):$(Build.BuildNumber) DbServer=$(DbServer) DbName=$(DbName) DbUID=$(DbUID) DbPWD=$(DbPWD) BuildNumber=$(Build.BuildNumber)
