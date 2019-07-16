bbe4355cea1e43280af088671f5bc50c27f92ad1
Zhenyu,Liu <liuzhenyulive@live.com>
2019-07-16 23:54:11 +0800

    Add template

--- Src/iBlogs.Site.Core/Git/GitBlogService.cs
+++ Src/iBlogs.Site.Core/Git/GitBlogService.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iBlogs.Site.Core.Common.Extensions;
 
 namespace iBlogs.Site.Core.Git
 {
     public class GitBlogService : IGitBlogService
     {
+        private static string template = "===============================================================\r\nTitle:\r\nTag:\r\nCategory:\r\n===============================================================\r\nLastUpdateTime:\r\nBlogId:\r\n===============================================================";
+
         public async Task<bool> Handle(string filePath)
         {
-            string fileText = null;
-            using (var reader = File.OpenText(filePath))
-            {
-                fileText = await reader.ReadToEndAsync();
-            }
+            string fileText = File.ReadAllText(filePath, Encoding.UTF8);
             fileText += DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            var lines = fileText.SplitToLines();
+            var firstLine = Array.IndexOf(lines, lines.FirstOrDefault(u => u.StartsWith("=") && u.EndsWith("=")));
+            var lastLine = Array.IndexOf(lines, lines.LastOrDefault(u => u.StartsWith("=") && u.EndsWith("=")));
+            if (firstLine == lastLine)
+                fileText += template;
+
             byte[] encodedText = Encoding.UTF8.GetBytes(fileText);
 
-            using (FileStream sourceStream = new FileStream(filePath,FileMode.Create, FileAccess.Write, FileShare.None,bufferSize: 4096, useAsync: true))
+            using (FileStream sourceStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
             {
                 await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
             };
