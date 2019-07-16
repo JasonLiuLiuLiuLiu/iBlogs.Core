45918042db78a6a2aae20af2086d9a8caff092d9
Zhenyu,Liu <liuzhenyulive@live.com>
2019-07-17 00:04:50 +0800

    Update GitBlogService.cs

--- Src/iBlogs.Site.Core/Git/GitBlogService.cs
+++ Src/iBlogs.Site.Core/Git/GitBlogService.cs
@@ -11,17 +11,17 @@ namespace iBlogs.Site.Core.Git
 {
     public class GitBlogService : IGitBlogService
     {
-        private static string template = "===============================================================\r\nTitle:\r\nTag:\r\nCategory:\r\n===============================================================\r\nLastUpdateTime:\r\nBlogId:\r\n===============================================================";
+        private static string template = "<===============================================================\r\nTitle:\r\nTag:\r\nCategory:\r\n===============================================================\r\nLastUpdateTime:\r\nBlogId:\r\n===============================================================>\r\n";
 
         public async Task<bool> Handle(string filePath)
         {
             string fileText = File.ReadAllText(filePath, Encoding.UTF8);
             fileText += DateTime.Now.ToString(CultureInfo.InvariantCulture);
             var lines = fileText.SplitToLines();
-            var firstLine = Array.IndexOf(lines, lines.FirstOrDefault(u => u.StartsWith("=") && u.EndsWith("=")));
-            var lastLine = Array.IndexOf(lines, lines.LastOrDefault(u => u.StartsWith("=") && u.EndsWith("=")));
-            if (firstLine == lastLine)
-                fileText += template;
+            var firstLine = Array.IndexOf(lines, lines.FirstOrDefault(u => u.StartsWith("<") && u.EndsWith("=")));
+            var lastLine = Array.IndexOf(lines, lines.LastOrDefault(u => u.StartsWith("=") && u.EndsWith(">")));
+            if (firstLine >= lastLine)
+                fileText = template + fileText;
 
             byte[] encodedText = Encoding.UTF8.GetBytes(fileText);
 
