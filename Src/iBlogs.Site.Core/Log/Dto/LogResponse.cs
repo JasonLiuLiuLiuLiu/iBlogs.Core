2b82ebf0707f5dfab8fa064856e9f24e6de02883
Zhenyu,Liu <liuzhenyulive@live.com>
2019-07-29 22:30:10 +0800

    Create LogResponse.cs

---
+++ Src/iBlogs.Site.Core/Log/Dto/LogResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace iBlogs.Site.Core.Log.Dto
+{
+    public class LogResponse
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Level { get; set; }
+
+        public string Template { get; set; }
+
+        public string Message { get; set; }
+
+        public string Exception { get; set; }
+
+        public string Properties { get; set; }
+
+    }
+}
