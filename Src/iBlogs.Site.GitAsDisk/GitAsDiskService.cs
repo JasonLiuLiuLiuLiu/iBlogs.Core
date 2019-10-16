e0f41050bf13b88c57e6661c5bf5ff575e179b08
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-16 23:19:47 +0800

    Add git as disk

---
+++ Src/iBlogs.Site.GitAsDisk/GitAsDiskService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace iBlogs.Site.GitAsDisk
+{
+    public class GitAsDiskService : IGitAsDiskService
+    {
+        public Task<bool> Sync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Commit<T>(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Load<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/iBlogs.Site.GitAsDisk/IGitAsDiskService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace iBlogs.Site.GitAsDisk
+{
+    public interface IGitAsDiskService
+    {
+        Task<bool> Sync();
+        Task<bool> Commit<T>(T value);
+        Task<T> Load<T>();
+    }
+}
