f7f68a2cac58d9486852e87d83ac28516a99d4b4
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-17 21:30:13 +0800

    update git as disk

--- Src/iBlogs.Site.GitAsDisk/GitAsDiskService.cs
+++ Src/iBlogs.Site.GitAsDisk/GitAsDiskService.cs
@@ -1,23 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace iBlogs.Site.GitAsDisk
 {
-    public class GitAsDiskService : IGitAsDiskService
+    public class GitAsDiskService
     {
-        public Task<bool> Sync()
+        private static bool _initiated;
+        private static string _basePath;
+
+        public static Task<bool> Sync(GitSyncOptions options)
         {
+            _initiated = true;
+            _basePath = options.Path;
             throw new NotImplementedException();
         }
 
-        public Task<bool> Commit<T>(T value)
+        public static async Task<bool> CommitAsync<T>(IEnumerable<T> values) where T : class
         {
-            throw new NotImplementedException();
+            if (!_initiated)
+            {
+                throw new GitAsDiskException("Please Call Sync Method before this");
+            }
+
+            StringBuilder resultSb = new StringBuilder();
+            foreach (var value in values)
+            {
+                resultSb.AppendLine(JsonConvert.SerializeObject(value).Replace("\r\n", " "));
+            }
+
+            return await CommitAsync(GetPathByType(typeof(T)), resultSb.ToString());
         }
 
-        public Task<T> Load<T>()
+        public static async Task<bool> CommitAsync(string fullPath, string value)
         {
-            throw new NotImplementedException();
+            if (!_initiated)
+            {
+                throw new GitAsDiskException("Please Call Sync Method before this");
+            }
+
+            var fileFullPath = Path.Combine(_basePath, fullPath);
+            await File.WriteAllTextAsync(fileFullPath, value, Encoding.UTF8);
+
+            return true;
+        }
+
+        public static async Task<string> LoadAsync(string fullPath)
+        {
+            if (!_initiated)
+            {
+                throw new GitAsDiskException("Please Call Sync Method before this");
+            }
+
+            return await File.ReadAllTextAsync(Path.Combine(_basePath, fullPath), Encoding.UTF8);
+        }
+
+        public static async IAsyncEnumerable<T> LoadAsync<T>() where T : class
+        {
+            if (!_initiated)
+            {
+                throw new GitAsDiskException("Please Call Sync Method before this");
+            }
+
+            var values = (await LoadAsync(GetPathByType(typeof(T)))).Split('\r', '\n');
+
+            foreach (var value in values)
+            {
+                yield return JsonConvert.DeserializeObject<T>(value);
+            }
+        }
+
+        private static string GetPathByType(Type T)
+        {
+            var typeFullName = T.FullName;
+
+            Debug.Assert(typeFullName != null, nameof(typeFullName) + " != null");
+
+            return typeFullName.Replace('.', '\\');
         }
     }
 }
---
+++ Src/iBlogs.Site.GitAsDisk/GitSyncOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace iBlogs.Site.GitAsDisk
+{
+    public class GitSyncOptions
+    {
+        public GitSyncOptions(string gitUrl, string uid, string pwd)
+        {
+            GitUrl = gitUrl;
+            UserName = uid;
+            Password = pwd;
+        }
+
+        public GitSyncOptions(string gitUrl, string uid, string pwd, string path) : this(gitUrl, uid, pwd)
+        {
+            Path = path;
+        }
+
+        public string Path { get; set; } = "GitAsDisk";
+        public string GitUrl { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
