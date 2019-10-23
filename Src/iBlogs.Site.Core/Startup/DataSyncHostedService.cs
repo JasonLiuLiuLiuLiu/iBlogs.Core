a09f23b5d23af485dfba5b2eae964f2525ed4184
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-23 21:51:55 +0800

    update data sync hosted service

--- Src/iBlogs.Site.Core/Startup/DataSyncHostedService.cs
+++ Src/iBlogs.Site.Core/Startup/DataSyncHostedService.cs
@@ -1,28 +1,35 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using iBlogs.Site.Core.Common.Extensions;
+using iBlogs.Site.GitAsDisk;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace iBlogs.Site.Core.Startup
 {
-    public class DataSyncHostedService:IHostedService
+    public class DataSyncHostedService : IHostedService
     {
         private readonly ILogger<DataSyncHostedService> _logger;
+        private readonly TimeSpan _sleepTimeSpan;
 
-        public DataSyncHostedService(ILogger<DataSyncHostedService> logger)
+        public DataSyncHostedService(ILogger<DataSyncHostedService> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _sleepTimeSpan = TimeSpan.FromHours(configuration?["AutoDataSyncHours"].ToIntOrDefault(1) ?? 1);
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Data Sync Hosted Service running.");
+            _logger.LogInformation("Data Sync Hosted Service Running.");
 
             while (!cancellationToken.IsCancellationRequested)
             {
-                //TODO
+                //TODO read git sync options from config
+                await GitAsDiskService.Sync(new GitSyncOptions("", "", "")).ConfigureAwait(false);
+                Thread.Sleep(_sleepTimeSpan);
             }
-            return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
