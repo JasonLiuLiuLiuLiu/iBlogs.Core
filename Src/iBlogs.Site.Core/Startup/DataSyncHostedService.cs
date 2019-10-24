cd127e8886781f69a85059c819a85a04e9e5f7ad
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-25 00:07:54 +0800

    Update DataSyncHostedService.cs

--- Src/iBlogs.Site.Core/Startup/DataSyncHostedService.cs
+++ Src/iBlogs.Site.Core/Startup/DataSyncHostedService.cs
@@ -2,17 +2,18 @@
 using System.Threading;
 using System.Threading.Tasks;
 using iBlogs.Site.Core.Common.Extensions;
-using iBlogs.Site.GitAsDisk;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace iBlogs.Site.Core.Startup
 {
-    public class DataSyncHostedService : IHostedService
+    public sealed class DataSyncHostedService : IHostedService, IDisposable
     {
         private readonly ILogger<DataSyncHostedService> _logger;
+        private  Timer _timer;
         private readonly TimeSpan _sleepTimeSpan;
+        private int executionCount = 0;
 
         public DataSyncHostedService(ILogger<DataSyncHostedService> logger, IConfiguration configuration)
         {
@@ -24,6 +25,14 @@ namespace iBlogs.Site.Core.Startup
         {
             _logger.LogInformation("Data Sync Hosted Service Running.");
 
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, _sleepTimeSpan);
+        }
+
+        private void DoWork(object state)
+        {
+            executionCount++;
+
+            _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", executionCount);
 
             //while (!cancellationToken.IsCancellationRequested)
             //{
@@ -31,13 +40,18 @@ namespace iBlogs.Site.Core.Startup
             //    //await GitAsDiskService.Sync(new GitSyncOptions("", "", "")).ConfigureAwait(false);
             //    Thread.Sleep(_sleepTimeSpan);
             //}
-
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Data Sync Hosted Service is stopping.");
+            _timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
         }
     }
 }
