ff09f684ba78a0d45c44b2bd0c4ee19090876d65
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-27 21:19:44 +0800

    update deploy script

--- Deploy/DeployForInstalled.sh
+++ Deploy/DeployForInstalled.sh
@@ -4,4 +4,4 @@ docker rm -f $(docker ps -a |  grep "$(imageName)*"  | awk '{print $1}')
 docker pull $(dockerId)/$(imageName):$(Build.BuildNumber)
 cd /
 mkdir iBlogsLog
-docker run -d -p 5000:80 --restart=always --name $(imageName) $(dockerId)/$(imageName):$(Build.BuildNumber) GitUrl=$(GitUrl) GitUid=$(GitUid) GitPwd=$(GitPWD) -v /iBlogsLog:/log
+docker run -d -p 5000:80 --restart=always --name $(imageName) $(dockerId)/$(imageName):$(Build.BuildNumber) GitUrl=$(GitUrl) GitUid=$(GitUid) GitPwd=$(GitPWD) -v /iBlogsLog:/app/log
