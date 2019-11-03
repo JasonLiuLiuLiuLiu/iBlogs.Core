e4f7ff3ecd4d07d976767d3fba0eb7607c78fc44
Zhenyu,Liu <liuzhenyulive@live.com>
2019-11-03 14:21:46 +0800

    add Error Log Notice Enricher

---
+++ Src/iBlogs.Site.Core/Git/ErrorLogNoticeEnricher.cs
@@ -0,0 +1,17 @@
+﻿using iBlogs.Site.Core.Log.Service;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace iBlogs.Site.Core.Git
+{
+    public class ErrorLogNoticeEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (logEvent.Level >= LogEventLevel.Error)
+            {
+               LogService.ErrorLogEventCallBack(logEvent);
+            }
+        }
+    }
+}
