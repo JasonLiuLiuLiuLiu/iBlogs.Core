bac529f7b364920e3b5efbd5bc721141a29a9bb4
Zhenyu,Liu <liuzhenyulive@live.com>
2020-05-08 23:03:24 +0800

    add exception ignore list

--- Src/iBlogs.Site.Core/Git/ErrorLogNoticeEnricher.cs
+++ Src/iBlogs.Site.Core/Git/ErrorLogNoticeEnricher.cs
@@ -1,4 +1,6 @@
-﻿using iBlogs.Site.Core.Log.Service;
+﻿using System.Linq;
+using iBlogs.Site.Core.Common.Extensions;
+using iBlogs.Site.Core.Log.Service;
 using Serilog.Core;
 using Serilog.Events;
 
@@ -10,8 +12,12 @@ namespace iBlogs.Site.Core.Git
         {
             if (logEvent.Level >= LogEventLevel.Error)
             {
-               LogService.ErrorLogEventCallBack(logEvent);
+                if (logEvent.Exception != null && ignoreList.Any(u => logEvent.Exception.Message.Contains(u)))
+                    return;
+                LogService.ErrorLogEventCallBack(logEvent);
             }
         }
+
+        private string[] ignoreList = new[] { "StatusCode cannot be set because the response has already started." };
     }
 }
